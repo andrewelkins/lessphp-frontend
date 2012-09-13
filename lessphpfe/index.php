1033224d52c7b73f772a00e82242bfb30ea6bf30
Andrew Elkins <accounts@andrewelkins.com>
2012-09-13 16:44:42 -0700

    Changed error message

--- lessphpfe/index.php
+++ lessphpfe/index.php
@@ -1,9 +1,9 @@
 <?php
 define('LESSPHPFE', 'handler.php');
-file_exists(LESSPHPFE) ? include LESSPHPFE : die('alert("lessphp-frontend Parse error: Can\'t find the lessphp-frontend class file: \''.LESSPHPFE.'\'.");');
+file_exists(LESSPHPFE) ? include LESSPHPFE : die('alert("Parse error: Can\'t find the lessphp-frontend handler class file: \''.LESSPHPFE.'\'.");');
 
 define('LESSPHPFECONFIG', 'config.php');
-file_exists(LESSPHPFECONFIG) ? include LESSPHPFECONFIG : die('alert("lessphp-frontend Parse error: Can\'t find the lessphp-frontend config file: \''.LESSPHPFECONFIG.'\'.");');
+file_exists(LESSPHPFECONFIG) ? include LESSPHPFECONFIG : die('alert("Parse error: Can\'t find the lessphp-frontend config file: \''.LESSPHPFECONFIG.'\'.");');
 
 $lessPhpFrontend = new lessphpfe_handler( new lessphpfe_config() );
 
